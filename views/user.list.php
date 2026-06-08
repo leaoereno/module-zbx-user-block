698a6249fcdd14e0688ff540317cf1606736a148
leaoereno <rafael.ereno@outlook.com>
2026-06-08 11:36:50 -0300

    fix: corrige assinatura onBeforeAction e refatora injeção de JS
    - Remove onBeforeAction (incompatível com Zabbix 7.0)
    - Simplifica Module.php usando apenas init() com CView::registerDirectory
    - Adiciona view user.list.php para injeção de JS na action nativa
    - Atualiza manifest.json declarando assets.js
    - Corrige namespace de CController para \CController global

---
+++ views/user.list.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Esta view é carregada automaticamente pelo Zabbix quando action=user.list
+ * Injeta o botão Bloquear via JavaScript
+ */
+?>
+<script src="<?= $this->getModule()->getDir() ?>/assets/js/block_user.js?v=1.0.0"></script>
